b6fee7496d85cb2fd3fe2960da7cb64c0193dc3e
Max <max.edison@yahoo.com>
2021-10-13 17:43:15 -0400

    commit tree 9dd67587c3f4ca6d494a35d1095865661369315d

---
+++ index.js
@@ -0,0 +1,12 @@
+// // const divide = (dividend, divisor) => dividend / divisor; 
+// const divide = (2000, 100) => 2000 / 100; 
+// divide
+// // divide(2000, 100);
+
+function divide (dividend = 2000, divisor = 100){
+    return dividend/divisor;
+}
+divide(2000,100);
+
+const square = x => x*x;
+const add = (num1, num2) => num1+num2;
